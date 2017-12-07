067fbc4df07a8eb96737a0c2440c8039b467027e
Tim Wong <twong@reliaslearning.com>
2017-12-07 13:06:52 -0500

    Day 7 solution (part 1)

--- Day7Solution/AdventOfCode2017/Program.cs
+++ Day7Solution/AdventOfCode2017/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace Day7Solution
 {
@@ -6,7 +8,30 @@ namespace Day7Solution
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string line = String.Empty;
+            var parentNodes = new HashSet<string>();
+            var childNodes = new HashSet<string>();
+            var file = new StreamReader(@".\Input\Day7Input.txt");
+
+            while((line = file.ReadLine()) != null)
+            {
+                if (line.Contains("->"))
+                {
+                    parentNodes.Add(line.Split(' ')[0]);
+                    childNodes.UnionWith(line.Substring(line.IndexOf("->") + 3).Split(", "));
+                }
+            }
+
+            foreach (var node in parentNodes)
+            {
+                if (!childNodes.Contains(node))
+                {
+                    Console.WriteLine($"The root node is [{node}]");
+                    break;
+                }
+            }
+
+            Console.ReadLine();
         }
     }
 }
