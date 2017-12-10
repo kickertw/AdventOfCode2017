02f9c4deb2dfe7c1f7c7109bef8d5c85e2e74391
Tim Wong <twong@reliaslearning.com>
2017-12-10 12:58:51 -0500

    Day 7 Part 2 solution

---
+++ Day7Solution/AdventOfCode2017/Model/Node.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Day7Solution.Model
+{
+    public class Node
+    {
+        public string Id { get; set; }
+        public string ParentId { get; set; }
+        public int Weight { get; set; }
+        public int Depth { get; set; }
+
+        public List<Node> Children { get; set; }
+
+        public Node()
+        {
+        }
+
+        public Node(string id, int weight, int depth, string parentId)
+        {
+            Id = id;
+            Weight = weight;
+            Depth = depth;
+            ParentId = parentId;
+        }
+
+        public Node FindById(string id)
+        {
+            Node retVal = null;
+
+            if (Id == id) return this;
+
+            if (Children != null)
+            {
+                foreach (var child in Children)
+                {
+                    retVal = child.FindById(id);
+                    if (retVal != null) break;
+                }
+            }
+
+            return retVal;
+        }
+
+        /// <summary>
+        /// Returns the weight of the node and the sum of it's child nodes
+        /// </summary>
+        /// <returns></returns>
+        public int GetTotalWeight()
+        {
+            var retVal = 0;
+
+            if (Children != null)
+            {
+                foreach (var child in Children)
+                {
+                    retVal += child.GetTotalWeight();
+                }
+            }
+
+            return retVal + Weight;
+        }
+
+        public static int GetMaxDepth(Node rootNode)
+        {
+            var depth = rootNode.Depth;
+            var baseNode = rootNode;
+            while (baseNode.Children != null)
+            {
+                depth = baseNode.Depth;
+                baseNode = baseNode.Children.FirstOrDefault();
+            }
+
+            return depth;
+        }
+    }
+}
--- Day7Solution/AdventOfCode2017/Program.cs
+++ Day7Solution/AdventOfCode2017/Program.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Day7Solution.Model;
+using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace Day7Solution
 {
@@ -9,29 +11,152 @@ namespace Day7Solution
         static void Main(string[] args)
         {
             string line = String.Empty;
-            var parentNodes = new HashSet<string>();
+            var allNodeWeights = new Dictionary<string, int>();   //key = node name, value = weight;
+            var allNodeParents = new Dictionary<string, List<string>>();
             var childNodes = new HashSet<string>();
+            //var file = new StreamReader(@".\Input\TestInput.txt");
             var file = new StreamReader(@".\Input\Day7Input.txt");
 
-            while((line = file.ReadLine()) != null)
-            {
+            while ((line = file.ReadLine()) != null) {
+                var splitLine = line.Split(' ');
+
+                allNodeWeights.Add(splitLine[0], int.Parse(splitLine[1].TrimStart('(').TrimEnd(')')));
                 if (line.Contains("->"))
                 {
-                    parentNodes.Add(line.Split(' ')[0]);
+                    allNodeParents.Add(splitLine[0], line.Substring(line.IndexOf("->") + 3).Split(", ").ToList());
                     childNodes.UnionWith(line.Substring(line.IndexOf("->") + 3).Split(", "));
                 }
             }
 
-            foreach (var node in parentNodes)
+            //Part 1
+            var rootNodeId = FindRootNode(allNodeParents, childNodes);
+            Console.WriteLine($"The root node is [{rootNodeId}]");
+
+            //Part 2
+            var flatNodeList = new List<Node>();
+            var rootNode = CreateNode(rootNodeId, allNodeWeights, allNodeParents, ref flatNodeList);
+            var correctWeight = CorrectTreeWeight(rootNode, flatNodeList);
+
+            Console.WriteLine($"The correct weight should be [{correctWeight}]");
+            Console.ReadLine();
+        }
+
+
+        private static Node CreateNode(string rootNodeId, Dictionary<string, int> nodeWeights, Dictionary<string, List<string>> nodeParents, ref List<Node> flatNodeList, int depth = 1, string parentId = "")
+        {
+            Node retVal = new Node(rootNodeId, nodeWeights[rootNodeId], depth, parentId);
+            flatNodeList.Add(retVal);
+
+            if (nodeParents.ContainsKey(rootNodeId))
             {
-                if (!childNodes.Contains(node))
+                retVal.Children = new List<Node>();
+                foreach (var childNode in nodeParents[rootNodeId])
                 {
-                    Console.WriteLine($"The root node is [{node}]");
-                    break;
+                    retVal.Children.Add(CreateNode(childNode, nodeWeights, nodeParents, ref flatNodeList, depth + 1, rootNodeId));
                 }
             }
 
-            Console.ReadLine();
+            return retVal;
+        }
+
+        // Find the Root Node (Part 1)
+        private static string FindRootNode(Dictionary<string, List<string>> parentNodes, HashSet<string> childNodes)
+        {
+            foreach (var node in parentNodes.Keys.ToArray())
+            {
+                if (!childNodes.Contains(node))
+                {
+                    return node;
+                }
+            }
+
+            throw new KeyNotFoundException();
+        }
+
+        // Find where there is an issue and what the weight correction would be
+        private static int CorrectTreeWeight(Node rootNode, List<Node> flatNodeList)
+        {
+            int? correctWeight = null;
+            var curDepth = Node.GetMaxDepth(rootNode);
+
+            // Should load all the weights for each node in the tree
+            rootNode.GetTotalWeight();
+
+            while (curDepth > 1)
+            {
+                var parentIds = flatNodeList.FindAll(i => i.Depth == curDepth).Select(i => i.ParentId).Distinct();
+
+                foreach (var parentId in parentIds)
+                {
+                    var node = rootNode.FindById(parentId);
+                    var badNode = WhichNodeIsBad(node.Children);
+
+                    if (badNode != null)
+                    {
+                        foreach(var child in node.Children)
+                        {
+                            if (child.Id != badNode.Id)
+                            {
+                                var delta = child.GetTotalWeight() - badNode.GetTotalWeight();
+                                correctWeight = badNode.Weight + delta;
+                                break;
+                            }
+                        }
+                    }
+
+                    if (correctWeight.HasValue) return correctWeight.Value;
+                }
+
+                curDepth--;
+            }
+
+            return int.MinValue;
+        }
+
+        private static Dictionary<string, int> FindNodeWeights(List<string> nodes, Dictionary<string, int> allNodes)
+        {
+            var retVal = new Dictionary<string, int>();
+
+            foreach (var nodeName in nodes)
+            {
+                retVal.Add(nodeName, allNodes[nodeName]);
+            }
+
+            return retVal;
+        }
+
+        private static Node WhichNodeIsBad(List<Node> nodeList)
+        {
+            Node badNode = null;
+
+            for(var ii = 1; ii < nodeList.Count && badNode == null; ii++)
+            {
+                if (nodeList[ii-1].GetTotalWeight() != nodeList[ii].GetTotalWeight())
+                {
+                    if (ii == 1)
+                    {
+                        if (nodeList[2].GetTotalWeight() == nodeList[0].GetTotalWeight())
+                        {
+                            badNode = nodeList[1];
+                        }
+                        else
+                        {
+                            badNode = nodeList[0];
+                        }                        
+                    }
+                    else
+                    {
+                        badNode = nodeList[ii];
+                    }
+                }
+            }
+
+            if (badNode != null)
+            {
+                Console.WriteLine($"Bad node found! [{badNode.Id}]");
+            }
+
+            return badNode;
         }
     }
 }
