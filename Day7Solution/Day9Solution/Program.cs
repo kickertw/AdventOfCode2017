88a82f6818d93407feb4ed27bd62ccf74aa9f9d4
Tim Wong <k1ck3rtw@gmail.com>
2017-12-10 22:38:28 -0500

    Day 9 solutions

---
+++ Day7Solution/Day9Solution/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+namespace Day9Solution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Should only be one line of text to read in
+            var input = File.ReadAllLines(@".\Input\Day9Input.txt");
+            var inp = input[0].ToCharArray();
+            var garbage = false;
+            int score = 0, depth = 1, garbageCount = 0;
+
+            for (var i = 0; i < inp.Count(); i++)
+            {
+                var c = inp[i];
+                if (c != '<' && !garbage) { Console.Write(c); }
+
+                if (c == '!') i++;
+                else if (garbage && c != '>') garbageCount++;
+                else if (c == '<') garbage = true;
+                else if (c == '>') garbage = false;
+                else if (c == '{') { Console.Write(c); score += depth++; }
+                else if (c == '}') { Console.Write(c); depth--; }
+            }
+
+            //Console.WriteLine(realString + " has score of " + score);
+            Console.WriteLine("Final Score = " + score);
+            Console.WriteLine("Garbage Char Count = " + garbageCount);
+            Console.ReadLine();
+        }
+    }
+}
