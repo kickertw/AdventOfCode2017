ffc740765fb7391c9fd47893c83bc1a9b629bf19
Tim Wong <twong@reliaslearning.com>
2017-12-14 10:35:04 -0500

    Day 13 solution (part 1)

--- Day7Solution/Day13Solution/Program.cs
+++ Day7Solution/Day13Solution/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
 
 namespace Day13Solution
 {
@@ -6,7 +8,67 @@ namespace Day13Solution
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var cursorIndex = 0;
+            var totalPenaltyCost = 0;
+            var input = File.ReadAllLines(@".\Input\Input.txt");
+
+            foreach (var line in input)
+            {
+                var depthRange = line.Split(": ").Select(i => int.Parse(i)).ToArray();
+                while (cursorIndex <= depthRange[0])
+                {
+                    if (IsCaught(cursorIndex, depthRange[0], depthRange[1]))
+                    {
+                        Console.WriteLine($"We got caught! Position {cursorIndex}");
+                        totalPenaltyCost += cursorIndex * depthRange[1];
+                    }
+                    
+                    cursorIndex++;
+                }
+            }
+
+            Console.WriteLine($"Total Penalty = {totalPenaltyCost}");
+            Console.ReadLine();
+        }
+
+        private static bool IsCaught(int cursorIndex, int depth, int range)
+        {
+            if (cursorIndex < depth)
+            {
+                Console.WriteLine($"CursorPosition = {cursorIndex} has no scanner");
+                return false;
+            }
+            else
+            {
+                // determine scanner location.  If the scanner ends at position 1, it was at position 0 which is the only way
+                // we get caught
+                var reverse = false;
+                var currentScannerIndex = 0;
+                for (var ii = 0; ii <= cursorIndex; ii++)
+                {
+                    //Console.WriteLine($"{ii}) Scanner start = {currentScannerIndex} going {(!reverse ? "forward" : "backward")}");
+
+                    // change direction
+                    if (currentScannerIndex == range - 1)
+                    {
+                        reverse = true;
+                    }
+                    else if (currentScannerIndex == 0)
+                    {
+                        reverse = false;
+                    }
+
+                    //increment
+                    if (reverse) { currentScannerIndex--; }
+                    else { currentScannerIndex++; }
+
+                    //Console.WriteLine($"{ii}) Scanner end = {currentScannerIndex} going {(!reverse ? "forward" : "backward")}");
+                }
+
+                //Console.WriteLine($"Scanner Index ended up at {currentScannerIndex} going {(!reverse ? "forward" : "backward")}");
+
+                return currentScannerIndex == 1 && !reverse;
+            }
         }
     }
 }
