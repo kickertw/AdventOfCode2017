25189df4a842714f7d532716eb615cb755951c2a
Tim Wong <twong@reliaslearning.com>
2017-12-15 16:30:18 -0500

    possible solution for part 2 of 13

--- Day7Solution/Day13Solution/Program.cs
+++ Day7Solution/Day13Solution/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
@@ -6,37 +7,46 @@ namespace Day13Solution
 {
     class Program
     {
+        static int delay = 0;
+        // key = ending scanner location, value = direction (true = reverse, false = forward)
+        static Dictionary<int, bool>[] firewall = new Dictionary<int, bool>[96];
+
         static void Main(string[] args)
         {
             var totalPenaltyCost = 1;
-            var delay = 0;
             var input = File.ReadAllLines(@".\Input\Input.txt");
+            var isCaught = true;
 
-            while (totalPenaltyCost > 0)
+            while (isCaught)
             {
+                isCaught = false;
                 var cursorIndex = 0;
                 totalPenaltyCost = 0;
 
+                //Console.WriteLine($"delay = {delay}");
                 foreach (var line in input)
                 {
                     var depthRange = line.Split(": ").Select(i => int.Parse(i)).ToArray();
                     while (cursorIndex <= depthRange[0])
                     {
-                        if (IsCaught(cursorIndex, depthRange[0], depthRange[1], delay))
+                        var caughtTemp = IsCaught(cursorIndex, depthRange[0], depthRange[1], delay);
+                        if (caughtTemp && !isCaught) isCaught = true;
+                        if (caughtTemp)
                         {
-                            Console.WriteLine($"We got caught! Position {cursorIndex}");
+                            //Console.WriteLine($"We got caught! Position {cursorIndex}");
                             totalPenaltyCost += cursorIndex * depthRange[1];
                         }
 
                         cursorIndex++;
                     }
                 }
+                //Console.WriteLine($"Delay {delay} / StillGotCaught = {isCaught}");
 
                 // Part 1
                 if (delay == 0) Console.WriteLine($"Total Penalty = {totalPenaltyCost}");
 
                 // Part 2
-                if (totalPenaltyCost == 0) Console.WriteLine($"Delay so no penalty = {delay}");
+                if (totalPenaltyCost == 0 && isCaught) Console.WriteLine($"Delay so no penalty = {delay}");
                 delay++;
             }
             
@@ -55,25 +65,55 @@ namespace Day13Solution
                 // we get caught
                 var reverse = false;
                 var currentScannerIndex = 0;
-                for (var ii = 0; ii <= cursorIndex + delay; ii++)
+
+                if (delay == 0)
                 {
-                    //Console.WriteLine($"{ii}) Scanner start = {currentScannerIndex} going {(!reverse ? "forward" : "backward")}");
-
-                    // change direction
-                    if (currentScannerIndex == range - 1)
+                    for (var ii = 0; ii <= cursorIndex; ii++)
                     {
-                        reverse = true;
-                    }
-                    else if (currentScannerIndex == 0)
-                    {
-                        reverse = false;
+                        //Console.WriteLine($"{ii}) Scanner start = {currentScannerIndex} going {(!reverse ? "forward" : "backward")}");
+
+                        // change direction
+                        if (currentScannerIndex == range - 1)
+                        {
+                            reverse = true;
+                        }
+                        else if (currentScannerIndex == 0)
+                        {
+                            reverse = false;
+                        }
+
+                        //increment
+                        if (reverse) { currentScannerIndex--; }
+                        else { currentScannerIndex++; }
+
+                        //Console.WriteLine($"{ii}) Scanner end = {currentScannerIndex} going {(!reverse ? "forward" : "backward")}");
                     }
 
-                    //increment
-                    if (reverse) { currentScannerIndex--; }
-                    else { currentScannerIndex++; }
+                    firewall[cursorIndex].Add(cursorIndex, reverse);
+                }
+                else
+                {
+                    for (var jj = 0; jj < firewall.Length; jj++){
+                        if (firewall[jj].Any())
+                        {
+                            var lastPosition = firewall[jj].First();
+                            firewall[jj].Clear();
 
-                    //Console.WriteLine($"{ii}) Scanner end = {currentScannerIndex} going {(!reverse ? "forward" : "backward")}");
+                            // change direction
+                            if (lastPosition.Key == range - 1)
+                            {
+                                reverse = true;
+                            }
+                            else if (lastPosition.Key == 0)
+                            {
+                                reverse = false;
+                            }
+
+                            //increment
+                            if (reverse) { firewall[jj].Add(lastPosition.Key - 1, reverse); }
+                            else { firewall[jj].Add(lastPosition.Key - 1, reverse); }
+                        }
+                    }
                 }
 
                 //Console.WriteLine($"Scanner Index ended up at {currentScannerIndex} going {(!reverse ? "forward" : "backward")}");
