39b7a5731e3d416c20e095dfc03c517370f06a70
Tim Wong <k1ck3rtw@gmail.com>
2017-12-18 21:58:47 -0500

    Day 4 part 2 solution

---
+++ Day7Solution/Day4Solution/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using System.Collections.Generic;
+
+namespace Day4Solution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = File.ReadAllLines(@".\input.txt");
+            var validCounter = 0;
+            foreach(var line in input)
+            {
+                if (IsValid(line))
+                {
+                    validCounter++;
+                }
+            }
+
+            Console.WriteLine("Valid Lines = " + validCounter);
+            Console.ReadLine();
+        }
+
+        static bool IsValid(string line)
+        {
+            var orderedPhrases = new HashSet<string>();
+            var passphrases = line.Split(' ');
+
+            foreach (var pwd in passphrases)
+            {
+                if (!orderedPhrases.Add(string.Concat(pwd.OrderBy(i => i))))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
