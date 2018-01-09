a9f91997a666bab5c8dab89b9e223cbb9ab8d06b
Tim Wong <twong@reliaslearning.com>
2018-01-09 17:40:35 -0500

    Day 21 continued

--- Day7Solution/Day21Solution/Program.cs
+++ Day7Solution/Day21Solution/Program.cs
@@ -2,15 +2,17 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text;
 
 namespace Day21Solution
 {
     class Program
     {
+        static Dictionary<string, string> instructions = new Dictionary<string, string>();
+
         static void Main(string[] args)
         {
-            var startingSquare = ".#./..#/###";
-            Dictionary<string, string> instructions = new Dictionary<string, string>();            
+            var startingSquare = ".#./..#/###";      
             string[] lines = File.ReadAllLines(".\\input.txt");
 
             // Get instructions and generate matching permutations (should be at most an additional 7)
@@ -21,7 +23,7 @@ namespace Day21Solution
 
                 // Rotate 3 times and add to the instructions
                 var alteredInput = kvp[0];
-                for(var ii = 1; ii < 3; ii++)
+                for(var ii = 1; ii < 4; ii++)
                 {
                     alteredInput = Rotate(alteredInput);
                     AddInstruction(ref instructions, alteredInput, kvp[1]);
@@ -29,52 +31,134 @@ namespace Day21Solution
 
                 // Flip and rotate 3 times and add to the instructions
                 alteredInput = Flip(kvp[0]);
-                instructions.Add(alteredInput, kvp[1]);
-                for (var ii = 1; ii < 3; ii++)
+                AddInstruction(ref instructions, alteredInput, kvp[1]);
+                for (var ii = 1; ii < 4; ii++)
                 {
                     alteredInput = Rotate(alteredInput);
                     AddInstruction(ref instructions, alteredInput, kvp[1]);
                 }
             }
 
-            var newSquare = startingSquare;
-            for (var ii = 0; ii < 1; ii++)
+            var nextSquare = startingSquare;
+            for (int ii = 0; ii < 2; ii++)
             {
-                var newSquareRows = newSquare.Split('/');
-                var squareSize = newSquareRows.Length;
+                nextSquare = GrowSquare(nextSquare);
 
-                if (squareSize <= 3)
+                Console.WriteLine("Showing square of size " + nextSquare.Split('/')[0].Length);
+                foreach (var row in nextSquare.Split('/'))
                 {
-                    newSquare = instructions[startingSquare];
+                    Console.WriteLine(row);
                 }
-                else 
-                {
-                    var totalSquares = 0;
-
-                    if (squareSize % 3 == 0)
-                    {
-
-                    }
-                    else
-                    {
-                        totalSquares = Convert.ToInt32(Math.Pow(squareSize / 2, 2));
-                        var splitSquares = new string[totalSquares];                // holds the mini-squares
-
-                        // Find the pattern and repeat (below is example of size 4 square)
-                        var currentRow = 0;
-                        splitSquares[0] = newSquareRows[currentRow].Substring(0, 2) + "/" + newSquareRows[currentRow + 1].Substring(0, 2);
-                        splitSquares[1] = newSquareRows[currentRow].Substring(2, 2) + "/" + newSquareRows[currentRow + 1].Substring(2, 2);
-                        currentRow += 2;
-                        splitSquares[2] = newSquareRows[currentRow].Substring(0, 2) + "/" + newSquareRows[currentRow + 1].Substring(0, 2);
-                        splitSquares[3] = newSquareRows[currentRow].Substring(2, 2) + "/" + newSquareRows[currentRow + 1].Substring(2, 2);
-
-                    }
-                }
+                Console.WriteLine();
             }
 
-            Console.WriteLine("There are " + newSquare.Count(i => i == '#') + " squares that are turned on");
             Console.WriteLine("Press enter to end program...");
             Console.ReadLine();
+        }
+
+        static string GrowSquare(string startingSquare)
+        {
+            var rows = startingSquare.Split('/');
+            if (rows.Length <= 3)
+            {
+                return instructions[startingSquare];
+            }
+            else
+            {
+                // Breaking up and reassembling a square 2 rows at a time
+                if (rows.Length % 2 == 0)
+                {
+                    var newSquareIndex = 0;
+                    var totalSquares = Convert.ToInt32(Math.Pow(rows.Length / 2, 2));
+                    var newSquare = new string[totalSquares];
+
+                    // Break up the larger square into smaller ones.  Then grow each smaller square
+                    for (var ii = 0; ii < rows.Length; ii += 2)
+                    {
+                        var firstRowSplit = Split(rows[ii], 2);
+                        var secondRowSplit = Split(rows[ii + 1], 2);
+
+                        for (var jj = 0; jj < rows.Length / 2; jj++)
+                        {
+                            newSquare[newSquareIndex++] = GrowSquare(firstRowSplit[jj] + "/" + secondRowSplit[jj]);
+                        }
+                    }
+
+                    // Reassemble each smaller square into one mega square
+                    // in single string notation
+                    var lastBlock = 0;
+                    StringBuilder newSquareStringFinal = new StringBuilder();
+                    StringBuilder newSquareString1 = new StringBuilder();
+                    StringBuilder newSquareString2 = new StringBuilder();
+
+                    for (var ii = 0; ii < rows.Length / 2; ii++)
+                    {
+                        for (var jj = 0; jj < rows.Length / 2; jj++)
+                        {
+                            var splitNS = newSquare[lastBlock + jj].Split('/');
+                            newSquareString1.Append(splitNS[0]);
+                            newSquareString2.Append(splitNS[1]);
+                        }
+                        lastBlock += rows.Length / 2;
+
+                        newSquareStringFinal.Append(newSquareString1).Append("/").Append(newSquareString2);
+                        if (ii < rows.Length - 1) { newSquareStringFinal.Append("/"); }
+                    }
+
+                    return newSquareStringFinal.ToString();
+                }
+                else // if (rows.Length % 3 == 0)
+                {
+                    var newSquareIndex = 0;
+                    var totalSquares = Convert.ToInt32(Math.Pow(rows.Length / 3, 2));
+                    var newSquare = new string[totalSquares];
+
+                    // Break up the larger square into smaller ones.  Then grow each smaller square
+                    for (var ii = 0; ii < rows.Length / 3; ii += 2)
+                    {
+                        var firstRowSplit = Split(rows[ii], 3);
+                        var secondRowSplit = Split(rows[ii + 1], 3);
+                        var thirdRowSplit = Split(rows[ii + 2], 3);
+
+                        for (var jj = 0; jj < rows.Length / 3; jj++)
+                        {
+                            newSquare[newSquareIndex++] = GrowSquare(GrowSquare(firstRowSplit[jj] + "/" + secondRowSplit[jj] + "/" + thirdRowSplit[jj]));
+                        }
+                    }
+
+                    // Reassemble each smaller square into one mega square
+                    // in single string notation
+                    var lastBlock = 0;
+                    StringBuilder newSquareStringFinal = new StringBuilder();
+                    StringBuilder newSquareString1 = new StringBuilder();
+                    StringBuilder newSquareString2 = new StringBuilder();
+                    StringBuilder newSquareString3 = new StringBuilder();
+
+                    for (var ii = 0; ii < rows.Length; ii++)
+                    {
+                        for (var jj = 0; jj < rows.Length / 3; jj++)
+                        {
+                            var splitNS = newSquare[lastBlock + jj].Split('/');
+                            newSquareString1.Append(splitNS[0]);
+                            newSquareString2.Append(splitNS[1]);
+                            newSquareString3.Append(splitNS[2]);
+                        }
+                        lastBlock += rows.Length / 3;
+
+                        newSquareStringFinal.Append(newSquareString1).Append("/").Append(newSquareString2).Append("/").Append(newSquareString3);
+                        if (ii < rows.Length - 1) { newSquareStringFinal.Append("/"); }
+                    }
+
+                    return newSquareStringFinal.ToString();
+                }
+            }
+        }
+
+        static string[] Split(string str, int chunkSize)
+        {
+            return Enumerable.Range(0, str.Length / chunkSize)
+                .Select(i => str.Substring(i * chunkSize, chunkSize))
+                .ToArray();
         }
 
         static void AddInstruction(ref Dictionary<string,string> instructions, string key, string val)
@@ -107,13 +191,13 @@ namespace Day21Solution
             // Add Key
             if (arraySize == 2)
             {
-                retVal = charRows[1, 0] + charRows[0, 0] + "/" + charRows[1, 1] + charRows[0, 1];
+                retVal = charRows[1, 0].ToString() + charRows[0, 0].ToString() + "/" + charRows[1, 1].ToString() + charRows[0, 1].ToString();
             }
             else
             {
-                retVal = charRows[2, 0] + charRows[1, 0] + charRows[0, 0] + "/" + 
-                         charRows[2, 1] + charRows[1, 1] + charRows[0, 1] + "/" +
-                         charRows[2, 2] + charRows[1, 2] + charRows[0, 2];
+                retVal = charRows[2, 0].ToString() + charRows[1, 0].ToString() + charRows[0, 0].ToString() + "/" + 
+                         charRows[2, 1].ToString() + charRows[1, 1].ToString() + charRows[0, 1].ToString() + "/" +
+                         charRows[2, 2].ToString() + charRows[1, 2].ToString() + charRows[0, 2].ToString();
             }
 
             // Rotate
